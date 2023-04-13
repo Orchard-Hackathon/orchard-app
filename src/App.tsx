863e821399627aa02e046d7319abd76eb1783222
a.kadyrov <a.kadyrov@vk.team>
2023-04-13 11:54:43 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  PanelHeader,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+import { MainComponent } from './Components/MainComponent';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppRoot>
+      <SplitLayout>
+        <SplitCol>
+          <MainComponent />
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
   );
-}
-
-export default App;
+};
---
+++ src/Components/MainComponent/index.tsx
@@ -0,0 +1,9 @@
+import { View, Panel } from '@vkontakte/vkui'
+
+export function MainComponent() {
+  return (
+    <View activePanel="main">
+      <Panel id="main">Main</Panel>
+    </View>
+  );
+}
